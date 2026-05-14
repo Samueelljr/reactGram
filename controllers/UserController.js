29b3c8e2ec65e1a7d2a15bde76c521a8af89d456
Samueelljr <samueldejesus705@gmail.com>
2026-05-14 12:50:02 -0300

    Middleware de upload de imagens + middleware de atualizar user + route/function de atualizar usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const mongoose = require('mongoose')
 
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
@@ -82,8 +83,48 @@ const getCurrentUser = async (req, res) => {
     res.status(200).json(user)
 }
 
+const update = async (req, res) => {
+    
+    const { name, password, bio } = req.body
+
+    let profileImage = null
+
+    if (req.file) {
+        profileImage = req.file.filename
+    }
+
+    const reqUser =  req.user
+
+    const user = await User.findById(new mongoose.Types.ObjectId(reqUser._id)).select("-password")
+
+    if (name) {
+        user.name = name
+    }
+
+    if (password) {
+        // generate password hash
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        user.password = passwordHash
+    }
+
+    if (profileImage) {
+        user.profileImage = profileImage
+    }
+
+    if (bio) {
+        user.bio = bio
+    }
+
+    await user.save()
+
+    res.status(200).json(user)
+}
+
 module.exports = {
     register,
     login,
-    getCurrentUser
+    getCurrentUser,
+    update
 }
